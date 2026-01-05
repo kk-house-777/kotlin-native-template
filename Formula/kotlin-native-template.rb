cd827d479190fcc27e61a896ccce07c99ea27778
kk-house-777 <intelinside.7575@gmail.com>
2026-01-05 18:47:21 +0900

    formula

--- Formula/kotlin-native-template.rb
+++ Formula/kotlin-native-template.rb
@@ -1,9 +1,9 @@
 class KotlinNativeTemplate < Formula
   desc "Kotlin Native project template generator"
   homepage "https://github.com/kk-house-777/kotlin-native-template"
-  url "https://github.com/kk-house-777/kotlin-native-template/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/kk-house-777/kotlin-native-template/archive/refs/tags/0.0.4.tar.gz"
   # curl -L https://github.com/kk-house-777/kotlin-native-template/archive/refs/tags/0.0.4.tar.gz | shasum -a 256
-  sha256 "" # リリース時に更新
+  sha256 "e0f755becef0ee2c0d8f5471e2c994bea4d1b113b22864d839cab3fcc455f9ff" # リリース時に更新
   license "MIT"
 
   def install
